702152596a31023db8628653d05828354993aee1
RyoMurakami <ryoyuki6@yahoo.co.jp>
2023-02-06 20:09:45 +0900

    p162

--- src/App.js
+++ src/App.js
@@ -5,15 +5,22 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      msg:'Hello Component!!!',
+      msg:'Hello!!!',
+      count:0,
     }
+    let timer = setInterval(()=>{
+      this.setState({
+        count: this.state.count + 1,
+        msg: "[ count: " + this.state.count + " ]"
+      })
+    }, 1000)
   }
 
   render(){
     return <div>
       <h1 className='bg-primary text-white display-4'>React!!!</h1>
       <div className='container'>
-        <p className='subtitle'>Show message!!!</p>
+        <p className='subtitle'>Count number!!!</p>
         <p className='alert alert-warning'>{this.state.msg}</p>
         <p className='alert alert-dark'>{this.props.msg}</p>
       </div>
