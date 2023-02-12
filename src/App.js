ff7fc6fb42495c41e6dc7bd90821319d202ebe65
Ryo Murakami <ryo@Ryo-MacBook-Pro.local>
2023-02-12 15:44:51 +0900

    p233

--- src/App.js
+++ src/App.js
@@ -1,54 +1,38 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function AlertMessage(props) {
-  const data = ["Hello!!!", "Welcome...", "Good-bye!!!"]
-
-  const actionAlert = ()=> {
-    const re = data[Math.floor(Math.random() * data.length)]
-    props.setAlert('message: "' + re + '".')
-  }
-
   return <div className='alert alert-primary h5 text-primary'>
-    <h5>{props.alert}</h5>
-    <button onClick={actionAlert} className="btn btn-primary">
-      Click me!!!
-    </button>
-  </div>
-}
-
-function CardMessage(props) {
-  const [count, setCount] = useState(0)
-
-  const actionCard = () => {
-    setCount(count + 1)
-    props.setCard("card counter: " + count + " count!!!")
-  }
-
-  return <div className='card p-3 border-primary text-center'>
-
-    <h5>{props.card}</h5>
-    <button onClick={actionCard} className="btn btn-secondary">
-      Click me!!!
-    </button>
+    <h5>{props.msg}</h5>
   </div>
 }
 
 function App() {
-  const [alert, setAlert] = useState("This is alert message!!!")
-  const [card, setCard] = useState("This is card message!!!")
+  const [val, setVal] = useState(0)
+  const [msg, setMsg] = useState('set a number...')
+
+  const doChange = (event) => {
+    setVal(event.target.value)
+  }
+
+  useEffect(() => {
+    let total = 0
+    for (let i = 0;i <= val;i++){
+      total += i
+    }
+    setMsg("total: " + total + "!!!")
+  })
 
   return (
     <div>
       <h1 className='bg-primary text-white display-4'>React!!!</h1>
       <div className='container'>
         <h4 className='my-3'>Hooks sample!!!</h4> 
-        <AlertMessage alert={alert} setAlert={setAlert} />
-        <CardMessage card={card} setCard={setCard} />
-        <ht />
-        <div className='text-right'>
-          <p>{alert}</p>
-          <p>{card}</p>
+        <AlertMessage msg={msg} />
+        <div className='form-group'>
+          <label>Input:</label>
+          <input type="number" className='form-contorol'
+            onChange={doChange} />
         </div>
       </div>
     </div>
