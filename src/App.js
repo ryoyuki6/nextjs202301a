52732f59cb178c93fcf9acdf1772cba092eeea6b
Ryo Murakami <ryo@Ryo-MacBook-Pro.local>
2023-02-12 12:34:49 +0900

    p219

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 import React, { useState } from 'react';
 import './App.css';
 
-function AlertMessage() {
+function AlertMessage(props) {
   return <div className='alert alert-primary h5 text-primary'>
-    This is Alert message!!!
+    {props.message}
   </div>
 }
 
-function CardMessage() {
+function CardMessage(props) {
   return <div className='card p-3 border-primary text-center'>
-    This is Card message!!!
+    {props.message}
   </div>
 }
 
 function App() {
+  const [msg, setMsg] = useState("This is sample message!!!")
+
+  const doAction = ()=>{
+    let res = window.prompt('type your name:')
+    setMsg("Hello, " + res + "!!")
+  }
+
   return (
     <div>
       <h1 className='bg-primary text-white display-4'>React!!!</h1>
       <div className='container'>
         <h4 className='my-3'>Hooks sample!!!</h4>
-        <AlertMessage />
-        <CardMessage />
+        <AlertMessage message={msg} />
+        <CardMessage message={msg} />
+        <div className='text-center'>
+          <button onClick={doAction} className="btn btn-primary">
+            Click me !!!
+          </button>
+        </div>
       </div>
     </div>
   )
